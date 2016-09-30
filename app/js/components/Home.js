a2400f5e2b359d2be69a6af05bdf1c3933786cad
KemCake <santos.remi@icloud.com>
2016-09-30 17:19:30 +0200

    Add social links

--- app/js/components/Home.js
+++ app/js/components/Home.js
@@ -4,9 +4,14 @@ export default class Home extends React.Component {
 
   render() {
     return (
-      <div id='container'>
-        <img id='emoji' src={require('images/emoji.png')} />
-        <h1 id='title'>That's it.</h1>
+      <div className='container'>
+        <img className='emoji' src={require('images/emoji.png')} />
+        <h1 className='title'>Hi there! I'm Remi</h1>
+        <div className='socials'>
+          <a target='_blank' href='https://github.com/kemcake/'><img src={require('images/github.png')}/></a>
+          <a target='_blank' href='https://twitter.com/kemcake/'><img src={require('images/twitter.png')}/></a>
+          <a target='_blank' href='https://instagram.com/remi_santos/'><img src={require('images/instagram.png')}/></a>
+        </div>
       </div>
     )
   }
