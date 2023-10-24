6e06a739f80a1e820710b1f1d69a95fb26606329
Remi Santos <santos.remi@icloud.com>
2023-10-24 16:17:55 +0200

    Add space

--- app/js/components/AppStoreTermsPrivacy.js
+++ app/js/components/AppStoreTermsPrivacy.js
@@ -168,7 +168,7 @@ These Services do not address anyone under the age of 13. I do not knowingly col
 <strong>Changes to This Privacy Policy</strong>
 <br/><br/>
 I may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Privacy Policy on this page.
-<br/><br/>
+<br/><br/><br/>
 This policy is effective as of 2023-09-12
     </p>)
   }
